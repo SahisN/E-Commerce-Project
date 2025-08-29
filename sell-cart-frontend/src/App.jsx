7a65dc62b2dad3ec5b56678d57e9c016aaada078
Sahis <sahisneupane23@gmail.com>
2025-08-28 22:14:45 -0700

    added react tools/depedencies

---
+++ sell-cart-frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { FaBeer } from "react-icons/fa";
+import "./App.css";
+
+function App() {
+  return (
+    <h1
+      className="flex items-center justify-center h-screen bg-gray-800 text-white 
+    text-4xl font-bold"
+    >
+      Welcome <FaBeer />
+    </h1>
+  );
+}
+
+export default App;
